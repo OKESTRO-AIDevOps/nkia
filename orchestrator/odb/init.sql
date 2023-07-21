ffc3ee49bce79645f7ce90d194f0ace890b7d13c
seantywork <seantywork@gmail.com>
2023-07-21 17:31:08 +0900

    orchestrator

--- orchestrator/odb/init.sql
+++ orchestrator/odb/init.sql
@@ -6,5 +6,16 @@ CREATE DATABASE orchestrator;
 
 USE orchestrator;
 
+CREATE TABLE orchestrator_pair (email_entry VARCHAR(100), amalgamated_config TEXT);
+
+
+
+SET @config_amld_enc = LOAD_FILE("/var/lib/mysql-files/config.amld");
+
+
+INSERT INTO orchestrator_pair(email_entry, amalgamated_config) VALUES("seantywork@gmail.com",@config_amld_enc);
+
+
+
 
 COMMIT;
