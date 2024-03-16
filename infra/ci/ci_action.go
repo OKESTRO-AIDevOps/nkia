f55ac00b3df31aaea08a37a696824cbd5264281e
seantywork <seantywork@gmail.com>
2024-03-16 17:59:25 +0900

    working infra, add staging logic in Makefile

--- infra/ci/ci_action.go
+++ infra/ci/ci_action.go
@@ -3,6 +3,7 @@ package ci
 import (
 	"fmt"
 
+	cigit "github.com/OKESTRO-AIDevOps/nkia/infra/git"
 	"gopkg.in/yaml.v3"
 )
 
@@ -22,28 +23,79 @@ func ActionV1(targets *CITargetsCtl, tidx int) {
 
 		log_line = fmt.Sprintf("failed to unmarshal playbook: %s", err.Error())
 
-		CILOCK.Lock()
-
-		targets.CI_TARGETS[tidx].CI_ERRLOG = append(targets.CI_TARGETS[tidx].CI_ERRLOG, log_line)
-
-		CILOCK.Unlock()
-
-		targets.CI_TARGETS[tidx].CI_STAT_FROM <- -1
+		LogErrAbortV1(targets, tidx, log_line)
 
 		return
 	}
 
 	log_line = fmt.Sprintf("successfully unmarshalled playbook for: version: %d, target: %d", vid, tid)
 
+	LogV1(targets, tidx, log_line)
+
+	repo_addr := target_ci.GitPackage.Address
+
+	repo_id := targets.CI_TARGETS[tidx].CI_USER_ID
+
+	repo_pw := targets.CI_TARGETS[tidx].CI_USER_PW
+
+	repo_nm := target_ci.GitPackage.Name
+
+	err = cigit.GetDestRepo(repo_addr, repo_id, repo_pw, repo_nm)
+
+	if err != nil {
+
+		log_line = fmt.Sprintf("failed to get dest repo: %s", err.Error())
+
+		LogErrAbortV1(targets, tidx, log_line)
+
+		return
+
+	}
+
+	log_line = "successfully fetched destination repo"
+
+	LogV1(targets, tidx, log_line)
+
+	err = QueryV1(&target_ci)
+
+	if err != nil {
+
+		log_line = fmt.Sprintf("failed to execute query v1: %s", err.Error())
+
+		LogErrAbortV1(targets, tidx, log_line)
+
+		return
+
+	}
+
+	log_line = "successfully executed query defined in CI description"
+
+	LogV1(targets, tidx, log_line)
+
+	targets.CI_TARGETS[tidx].CI_STAT_FROM <- SIG_SUCCESS
+
+	return
+
+}
+
+func LogV1(targets *CITargetsCtl, tidx int, log_line string) {
+
 	CILOCK.Lock()
 
 	targets.CI_TARGETS[tidx].CI_LOG = append(targets.CI_TARGETS[tidx].CI_LOG, log_line)
 
 	CILOCK.Unlock()
-	//CILOCK.Unlock()
 
-	targets.CI_TARGETS[tidx].CI_STAT_FROM <- SIG_SUCCESS
+}
 
-	return
-	//time.Sleep(1000 * time.Second)
+func LogErrAbortV1(targets *CITargetsCtl, tidx int, log_line string) {
+
+	CILOCK.Lock()
+
+	targets.CI_TARGETS[tidx].CI_ERRLOG = append(targets.CI_TARGETS[tidx].CI_ERRLOG, log_line)
+
+	CILOCK.Unlock()
+
+	targets.CI_TARGETS[tidx].CI_STAT_FROM <- SIG_FAILED
+
 }
